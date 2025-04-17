fd8a742825cac830b0606595ae73aef299fe0bf9
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2025-04-17 10:17:24 +0100

    Update stracture

---
+++ Backend/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\api\TaskController;
+use App\Http\Controllers\api\TasklistController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Route::resource('tasklists', \App\Http\Controllers\Api\TaskController::class);
+
+Route::resource('tasklists', TasklistController::class);
+Route::resource('tasks', TaskController::class)->except(['edit']);
+Route::post('tasks/{id}/edit', [TaskController::class, 'update']);
+
+
+
